7fcbae4c5028d01d860acc1f6ed73e617c00b378
Szymon Duchniewicz <szduchniewicz@gmail.com>
2021-11-29 18:35:02 +0000

    boards: arm: wio_terminal: Minimise board description
    - Add Copyright tags.
    - Remove commented out code.
    - Remove obsolete files.
    - Update yaml file (supported features).
    
    Signed-off-by: Szymon Duchniewicz <szduchniewicz@gmail.com>

--- boards/arm/wio_terminal/board.cmake
+++ boards/arm/wio_terminal/board.cmake
@@ -1,4 +1,5 @@
 # Copyright (c) 2020 Google LLC.
+# Copyright (c) 2021 Szymon Duchniewicz
 # SPDX-License-Identifier: Apache-2.0
 
 include(${ZEPHYR_BASE}/boards/common/bossac.board.cmake)
--- boards/arm/wio_terminal/pinmux.c
+++ boards/arm/wio_terminal/pinmux.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2020 Google LLC.
+ * Copyright (c) 2021 Szymon Duchniewicz
  *
  * SPDX-License-Identifier: Apache-2.0
  */
@@ -19,39 +20,6 @@ static int board_pinmux_init(const struct device *dev)
 	ARG_UNUSED(dev);
 	ARG_UNUSED(muxa);
 	ARG_UNUSED(muxb);
-
-#if ATMEL_SAM0_DT_SERCOM_CHECK(1, atmel_sam0_spi) && defined(CONFIG_SPI_SAM0)
-	/* SPI SERCOM1 on MISO=PB23/pad 3, MOSI=PA0/pad 0, SCK=PA1/pad 1 */
-	pinmux_pin_set(muxa, 0, PINMUX_FUNC_D);
-	pinmux_pin_set(muxa, 1, PINMUX_FUNC_D);
-	pinmux_pin_set(muxb, 23, PINMUX_FUNC_C);
-#endif
-
-#if ATMEL_SAM0_DT_SERCOM_CHECK(0, atmel_sam0_spi) && defined(CONFIG_SPI_SAM0)
-#warning Pin mapping may not be configured
-#endif
-#if ATMEL_SAM0_DT_SERCOM_CHECK(2, atmel_sam0_spi) && defined(CONFIG_SPI_SAM0)
-#warning Pin mapping may not be configured
-#endif
-#if ATMEL_SAM0_DT_SERCOM_CHECK(4, atmel_sam0_spi) && defined(CONFIG_SPI_SAM0)
-#warning Pin mapping may not be configured
-#endif
-#if ATMEL_SAM0_DT_SERCOM_CHECK(5, atmel_sam0_spi) && defined(CONFIG_SPI_SAM0)
-#warning Pin mapping may not be configured
-#endif
-
-#if defined(CONFIG_PWM_SAM0_TCC)
-#if ATMEL_SAM0_DT_TCC_CHECK(0, atmel_sam0_tcc_pwm)
-	/* TCC0/WO[2] on PA22 (LED) */
-	pinmux_pin_set(muxa, 22, PINMUX_FUNC_G);
-#endif
-#if ATMEL_SAM0_DT_TCC_CHECK(1, atmel_sam0_tcc_pwm)
-	/* TCC1/WO[2] on PA18 (D7) */
-	pinmux_pin_set(muxa, 18, PINMUX_FUNC_F);
-	/* TCC1/WO[3] on PA19 (D9) */
-	pinmux_pin_set(muxa, 19, PINMUX_FUNC_F);
-#endif
-#endif
 
 	if (IS_ENABLED(CONFIG_USB_DC_SAM0)) {
 		/* USB DP on PA25, USB DM on PA24 */
